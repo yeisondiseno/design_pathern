af61a9e1e2cca0cd2ecf0df184619165bd3104fa
Yeison Montoya <yeison.montoya@pragma.com.co>
2022-08-12 17:15:50 -0500

    Y.Montoya: extends and inheritance

--- intro/js/intro.js
+++ intro/js/intro.js
@@ -70,3 +70,18 @@ console.log(drink.info());
 
 const drink2 = new Drink2("Cerveza");
 console.log(drink2.info());
+
+// herencia
+class Beer extends Drink {
+  constructor(name, alcohol) {
+    super(name);
+    this.alcohol = alcohol;
+  }
+
+  info() {
+    return `${super.info()} ${this.alcohol}`;
+  }
+}
+
+const beer = new Beer("Erdinger", 8.5);
+console.log(beer.info());
