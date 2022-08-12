9ae83a38ad1d5e32c84e612178d41974ea25c702
Yeison Montoya <yeison.montoya@pragma.com.co>
2022-08-12 08:54:36 -0500

    Y.Montoya: reduce

--- intro/js/intro.js
+++ intro/js/intro.js
@@ -27,3 +27,23 @@ let fA = () => {
 fA();
 
 operation((a, b) => a * b, 5, 7);
+
+/// foreach
+const names = ["Yeison", "Jenis", "Juan", "Anna"];
+names.forEach((name) => console.log(name));
+names.forEach((name) => console.log(name.toLocaleUpperCase()));
+console.log("names ", names);
+names.sort(); // este es mutable
+console.log("names sorted ", names);
+
+// map
+const namesUpper = names.map((name) => name.toLocaleUpperCase());
+console.log("namesUpper ", namesUpper);
+
+// reduce
+const numbers = [5, 4, 7, 1, 10];
+const total = numbers.reduce((ac, number) => {
+  console.log("ac", ac);
+  return ac + number * 2;
+}, 0); // el valor despues de la coma es el valor inicial del acumulado
+console.log("total", total);
