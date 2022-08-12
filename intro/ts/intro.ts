bb75e44740dfdf177506f4dc9c5c66df4001f609
Yeison Montoya <yeison.montoya@pragma.com.co>
2022-08-12 17:30:14 -0500

    Y.Montoya: typeScript

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -1,1 +1,34 @@
-console.log("soy type ts 2");
+import { clearScreenDown } from "readline";
+
+class DrinkT {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  info(): string {
+    return this.name
+  }
+}
+
+let drinkT = new DrinkT('Agua');
+console.log(drinkT.info()); // this is protected
+
+
+// herencia
+class BearT extends DrinkT {
+  private alcohol: number;
+
+  constructor(name: string, alcohol: number ){
+    super(name);
+    this.alcohol = alcohol;
+  }
+
+  info(): string {
+    return `${super.info()} ${this.alcohol}`;
+  }
+}
+
+const beerT = new BearT("Erdinger", 8.5);
+console.log(beerT.info());
