1b59f2f8b16480a07495090988d53bce8ff8a262
Yeison Montoya <yeison.montoya@pragma.com.co>
2023-02-20 16:05:24 -0500

    Y.Montoya: singeltone examples

--- singleton/js/singleton.js
+++ singleton/js/singleton.js
@@ -14,11 +14,53 @@ class Singleton {
   }
 }
 
-const singleton = new Singleton();
-const singleton2 = new Singleton();
-const singleton3 = Singleton.getInstance();
-console.log(singleton.random);
-console.log(singleton2.random);
-console.log(singleton3.random);
-console.log(singleton === singleton2);
-console.log(singleton3 === singleton2);
+// const singleton = new Singleton();
+// const singleton2 = new Singleton();
+// const singleton3 = Singleton.getInstance();
+// console.log(singleton.random);
+// console.log(singleton2.random);
+// console.log(singleton3.random);
+// console.log(singleton === singleton2);
+// console.log(singleton3 === singleton2);
+
+class WeekDays {
+  dayEs = [
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+    "Doming",
+  ];
+
+  dayEn = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  constructor(lang) {
+    this.lang = lang;
+
+    if (WeekDays.instance) {
+      return WeekDays.instance;
+    }
+
+    WeekDays.instance = this;
+  }
+
+  getDays() {
+    return this.lang === "es" ? this.dayEs : this.dayEn;
+  }
+}
+
+const weekDays = new WeekDays("es");
+const weekDays2 = new WeekDays("en");
+
+console.log(weekDays.getDays());
+console.log(weekDays2.getDays());
